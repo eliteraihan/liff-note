895f0502d516c63282c6a78449fa9864efe56c48
eliteRaihan <elite.raihan@gmail.com>
2020-12-26 15:21:14 +0700

    fungsi notifikasi alert dan toggle element (153-5614)

--- liff-starter.js
+++ liff-starter.js
@@ -156,3 +156,25 @@ function registerButtonHandlers() {
         }
     });
 }
+
+/**
+ * Untuk menampilkan pesan di layar yang menandakan
+ * aplikasi LIFF tidak mendukung eksternal browser.
+ */
+function sendAlertIfNotInClient() {
+    alert('This button is unavailable as LIFF is currently being opened in an external browser.');
+}
+
+/**
+* Toggle specified element.
+* untuk beralih dari satu elemen ke elemen yang lainnya.
+* @param {string} elementId The ID of the selected element
+*/
+function toggleElement(elementId) {
+    const elem = document.getElementById(elementId);
+    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+        elem.style.display = 'none';
+    } else {
+        elem.style.display = 'block';
+    }
+}
